b238147cf191e57ce294f359ba2a9f4ea8ab1bd1
mohdsahil36 <mohdsahilsiddiqui36@gmail.com>
2024-02-23 12:10:45 +0530

    code update

--- src/components/Modal-2/Modal2.jsx
+++ src/components/Modal-2/Modal2.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from 'react';
+import classes from './Modal2.module.css';
 
 export default function Modal2(){
     return(
         <h2>
-            modal2
+            <div className={`container p-5 ${classes['modal-box']}`}>
+                <h3 className={classes.step}>Step 1</h3>
+                <h2 className={classes.yourProfile}>Business Information</h2>
+                <p className={classes['sub-text']}>Please enter the information about your company</p>
+            </div>
         </h2>
     )
 }
